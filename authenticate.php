3653d6e8a181d89682e102859e1df5abd1fe858f
JonSkenderis <jonskenderis@gmail.com>
2024-01-04 19:19:42 +0200

    back btn on login/register

--- authenticate.php
+++ authenticate.php
@@ -61,7 +61,7 @@ if (isset($_POST['login'])) {
     $email = sanitize($_POST['email']);
     $password = $_POST['password'];
 
-    $conn=connect2db();
+    $conn = connect2db();
 
     // Retrieve user from the database based on the email
     $stmt = $conn->prepare("SELECT * FROM user WHERE email = ?");
@@ -96,6 +96,7 @@ if ($_SESSION['role'] == 1) {
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -113,98 +114,87 @@ if ($_SESSION['role'] == 1) {
     <link rel="stylesheet" href="css/login-register.css">
 
     <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
-            integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
-            crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
-            integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
-            crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 
     <title>Calendar #10</title>
 </head>
+
 <body>
+    <div class="section">
+        <div class="container">
+            <div class="row full-height justify-content-center">
+                <div class="col-12 text-center align-self-center py-5">
+                    <div class="section pb-5 pt-5 pt-sm-2 text-center">
+                        <h6 class="mb-0 pb-3"><span>Log In </span><span>Register</span></h6>
+                        <input class="checkbox" type="checkbox" id="reg-log" name="reg-log" />
+                        <label for="reg-log"></label>
+                        <div class="card-3d-wrap mx-auto">
+                            <div class="card-3d-wrapper">
 
-<a href="https://front.codes/" class="logo" target="_blank">
-    <img src="https://assets.codepen.io/1462889/fcy.png" alt="">
-</a>
-
-<div class="section">
-    <div class="container">
-        <div class="row full-height justify-content-center">
-            <div class="col-12 text-center align-self-center py-5">
-                <div class="section pb-5 pt-5 pt-sm-2 text-center">
-                    <h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
-                    <input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
-                    <label for="reg-log"></label>
-                    <div class="card-3d-wrap mx-auto">
-                        <div class="card-3d-wrapper">
-
-                            <div class="card-front">
-                                <div class="center-wrap">
-                                    <div class="section text-center">
-                                        <h4 class="mb-4 pb-3">Log In</h4>
-                                        <form action="authenticate.php" method="post">
-                                        <div class="form-group">
-                                            <input type="email" name="email" class="form-style"
-                                                   placeholder="Your Email" id="email" autocomplete="off">
-                                            <i class="input-icon uil uil-at"></i>
+                                <div class="card-front">
+                                    <div class="center-wrap">
+                                        <div class="section text-center">
+                                            <h4 class="mb-4 pb-3">Log In</h4>
+                                            <form action="authenticate.php" method="post">
+                                                <div class="form-group">
+                                                    <input type="email" name="email" class="form-style" placeholder="Your Email" id="email" autocomplete="off">
+                                                    <i class="input-icon fas fa-at"></i>
+                                                </div>
+                                                <div class="form-group mt-2">
+                                                    <input type="password" name="password" class="form-style" placeholder="Your Password" id="pass" autocomplete="off">
+                                                    <i class="input-icon fas fa-lock"></i>
+                                                </div>
+                                                <button name="login" value="login" class="btn mt-4">submit</button>
+                                                <a href="index.php" class="btn mt-4"><i class="fas fa-long-arrow-alt-left"></i>back</a>
+                                                <p class="mb-0 mt-4 text-center"><a href="#0" class="link">Forgot your
+                                                        password?</a></p>
+                                            </form>
                                         </div>
-                                        <div class="form-group mt-2">
-                                            <input type="password" name="password" class="form-style"
-                                                   placeholder="Your Password" id="pass" autocomplete="off">
-                                            <i class="input-icon uil uil-lock-alt"></i>
-                                        </div>
-                                        <button name="login" value="login" class="btn mt-4">submit</button>
-                                        <p class="mb-0 mt-4 text-center"><a href="#0" class="link">Forgot your
-                                                password?</a></p>
-                                        </form>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="card-back">
-                                <div class="center-wrap">
-                                    <div class="section text-center">
-                                        <h4 class="mb-4 pb-3">Register</h4>
-                                        <form action="authenticate.php" method="post">
-                                            <div class="form-group">
-                                                <input type="text" name="name" class="form-style" value="<?php echo htmlspecialchars($name); ?>"
-                                                       placeholder="Your Full Name" id="name" autocomplete="off">
-                                                <i class="input-icon uil uil-user"></i>
-                                            </div>
-                                            <div class="form-group mt-2">
-                                                <input type="email" name="email" class="form-style" value="<?php echo htmlspecialchars($email); ?>"
-                                                       placeholder="Your Email" id="email" autocomplete="off">
-                                                <i class="input-icon uil uil-at"></i>
-                                            </div>
-                                            <div class="form-group mt-2">
-                                                <input type="password" name="password" class="form-style"
-                                                       placeholder="Your Password" id="password" autocomplete="off" value="<?php echo htmlspecialchars($password); ?>">
-                                                <i class="input-icon uil uil-lock-alt"></i>
-                                            </div>
-                                            <div class="form-group mt-2">
-                                                <input type="text" name="dept" class="form-style"
-                                                       placeholder="Your Department" id="dept" autocomplete="off" value="<?php echo htmlspecialchars($dept); ?>">
-                                                <i class="input-icon uil uil-lock-alt"></i>
-                                            </div>
-                                            <button name="register" value="register" class="btn mt-4" type="submit">submit</button>
-                                        </form>
+                                <div class="card-back">
+                                    <div class="center-wrap">
+                                        <div class="section text-center">
+                                            <h4 class="mb-4 pb-3">Register</h4>
+                                            <form action="authenticate.php" method="post">
+                                                <div class="form-group">
+                                                    <input type="text" name="name" class="form-style" value="<?php echo htmlspecialchars($name); ?>" placeholder="Your Full Name" id="name" autocomplete="off">
+                                                    <i class="input-icon fas fa-id-card"></i>
+                                                </div>
+                                                <div class="form-group mt-2">
+                                                    <input type="email" name="email" class="form-style" value="<?php echo htmlspecialchars($email); ?>" placeholder="Your Email" id="email" autocomplete="off">
+                                                    <i class="input-icon fas fa-at"></i>
+                                                </div>
+                                                <div class="form-group mt-2">
+                                                    <input type="password" name="password" class="form-style" placeholder="Your Password" id="password" autocomplete="off" value="<?php echo htmlspecialchars($password); ?>">
+                                                    <i class="input-icon fas fa-lock"></i>
+                                                </div>
+                                                <div class="form-group mt-2">
+                                                    <input type="text" name="dept" class="form-style" placeholder="Your Department" id="dept" autocomplete="off" value="<?php echo htmlspecialchars($dept); ?>">
+                                                    <i class="input-icon fas fa-building"></i>
+                                                </div>
+                                                <button name="register" value="register" class="btn mt-4" type="submit">submit</button>
+                                                <a href="index.php" class="btn mt-4"><i class="fas fa-long-arrow-alt-left"></i>back</a>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 
 </body>
+
 </html>
